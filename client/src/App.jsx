05bee25c1a6145825c4d9234a1b35d7d9c148207
Arjun <arjungehlot552@gmail.com>
2023-09-30 00:26:31 +0530

    small paragraph add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   console.log("aap se mil kr aacha lgaaa")
   return (
     <>
-
+      <h1>Hwllo</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora magnam nam deserunt ipsa dignissimos? Nostrum recusandae expedita quia vero eveniet.</p>
     </>
 
   )
