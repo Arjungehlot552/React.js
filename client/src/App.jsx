574aa75dfae31481d2f18eeb54e44bde9ba350fb
Arjun <arjungehlot552@gmail.com>
2023-09-30 00:30:33 +0530

    new add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Hwllo</h1>
+      <h1>Somthing is happen for some resion</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora magnam nam deserunt ipsa dignissimos? Nostrum recusandae expedita quia vero eveniet.</p>
     </>
 
